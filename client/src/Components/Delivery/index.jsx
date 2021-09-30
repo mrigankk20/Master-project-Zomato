55f1dbe2d634097f82f8e40fe8b52494c1368d14
hp <mrigank.mathur3111@gmail.com>
2021-09-30 21:34:51 +0530

    Delivery ready

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React,{useState} from "react";
+import RestaurantCard from "../RestaurantCard";
+import Brand from "./Brand";
 import DeliveryCarousel from "./DeliveryCarousel";
 
+
 const Delivery = () => {
+    const[restaurantList, setRestaurantList] = useState([
+        {
+            _id:"123456",
+            photos:["https://b.zmtcdn.com/data/pictures/chains/5/55/d16abff4c17aa648587e06af24505863_o2_featured_v2.jpg"],
+            name:"Berco's",
+            cuisine:["Chinese", "Thai", "Asian", "Beverages"],
+            averageCost:100,
+            isPro:true,
+            isOff: 80,
+            durationOfdelivery: 45,
+            restaurantReviewValue: 4.1,
+
+        },
+        {
+            _id:"124567",
+            photos:["https://b.zmtcdn.com/data/pictures/chains/1/491/fba01c7a0a33fe1d4b2a48ad364e55ec_o2_featured_v2.jpg"],
+            name:"Bikanervala",
+            cuisine:["Mithai", "North Indian", "South Indian", "Chinese", "Fast Food", "Street Food",],
+            averageCost:200,
+            isPro:true,
+            isOff: 70,
+            durationOfdelivery: 30,
+            restaurantReviewValue: 4.2,
+            
+        },
+        {
+            _id:"124567",
+            photos:["https://b.zmtcdn.com/data/pictures/7/313267/63bf34b2267e6d0777d58be89f0c760b_o2_featured_v2.jpg"],
+            name:"For God's Cake",
+            cuisine:["Bakery"],
+            averageCost:200,
+            isPro:true,
+            isOff: 70,
+            durationOfdelivery: 30,
+            restaurantReviewValue: 4.2,
+            
+        },
+    ]);
     return (
         <>
             <DeliveryCarousel />
+            <Brand />
+            <div className="flex justify-between flex-wrap">
+                {restaurantList.map((restaurant) =>(
+                    <RestaurantCard {...restaurant} key ={restaurant._id} />
+                ))}
+            </div>
         </>
     );
 };
---
+++ client/src/Components/RestaurantCard.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { AiTwotoneStar } from "react-icons/ai";
+
+const RestaurantCard = (props) => {
+    return (
+        <>
+            <div className="bg-white p-4 w-full mb-5 rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/3 ">
+                <div className="w-full h-56 lg:h-64 relative rounded-2xl">
+                    <div className="absolute flex items-end justify-between w-full bottom-4">
+                        <div className="flex flex-col gap-2 items-start">
+                            {props.isPro && (
+                            <span className="bg-zomato-600 text-white px-2 py-1 rounded text-sm">
+                                Pro Extra 10% off
+                            </span>
+                            )}
+                            {props.isOff && (
+                            <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">
+                                ₹{`${props.isOff}`} OFF
+                            </span>
+                            )}
+                        </div>
+                        <div>
+                            <span className="bg-white bg-opacity-50 p-1 rounded mr-3">
+                                {props.durationOfdelivery} min
+                            </span>
+                        </div>
+                    </div>
+                    <img
+                        src={props.photos.length && props.photos[0]}
+                        //src="https://b.zmtcdn.com/data/pictures/7/19355277/425a2665372f7d68dcb8e27be4db425e_o2_featured_v2.jpg"
+                        alt="restaurant"
+                        className="w-full h-full rounded-2xl"
+                    />
+                </div>
+                <div className="my-2 flex flex-col gap-2">
+                    <div className="flex items-center justify-between">
+                        <h4 className="text-xl font-medium">{props.name}</h4>
+                        <span className="bg-green-800 text-sm text-white p-1 flex items-center rounded">
+                            {props.restaurantReviewValue}
+                            <AiTwotoneStar />
+                        </span>
+                    </div>
+                    <div className="flex items-center justify-between text-gray-500">
+                        <p>{props.cuisine.join(", ")}</p>
+                        <p>₹ {props.averageCost} for one</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default RestaurantCard;
